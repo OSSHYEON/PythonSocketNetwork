e365f0f113df666d58a302f97f2204238303d85d
OSSHYEON <osh5237@naver.com>
2022-03-04 10:49:55 +0900

    함수 설명 주석 추가

--- client/client.py
+++ client/client.py
@@ -9,23 +9,28 @@ class Signal(QObject):
     disconnect_signal = pyqtSignal()
 
 class ClientSocket():
+    """
+    부모 클래스 윈도우 에서 버튼을 클릭하면, 데이터를 받아와 소켓 통신을 실행하는 클래스
+    client_ui.py 파일의 client_window 클래스 실행 시 호출된다.
+    :__init__: 부모클래스인 ui, 데이터 수신 시그널, 연결 끊김 시그널 선언
+    """
 
-    def __init__(self, parent):
-        self.parent = parent
+    def __init__(self, parent): # 라인 생성자 함수
+        self.parent = parent    # 부모 윈도우 저장할 self.parent 변수 선언
         self.bConnect = False
-        self.recv = Signal()
+        self.recv = Signal()    # 데이터 수신 시그널 선언
         self.recv.recv_signal.connect(self.parent.updateMsg)
-        self.disconnect = Signal()
+        self.disconnect = Signal()   # 연결 끊길 때 사용할 시그널 선언
         self.disconnect.disconnect_signal.connect(self.parent.updateDisconnect)
 
     def __del__(self):
         self.stop()
 
-    def connectServer(self, ip, port):
+    def connectServer(self, ip, port): # 접속 버튼 눌렀을 때 호출되는 함수. 소켓 생성, 해당 IP 주소의 포트 번호로 연결 시도
         self.client = socket(AF_INET, SOCK_STREAM)
 
         try:
-            self.client.connect((ip, port))
+            self.client.connect((ip, port))    #
 
         except Exception as e:
             print('Connect Error : ', e)
@@ -39,14 +44,21 @@ class ClientSocket():
 
         return True
 
-    def stop(self):
+    def stop(self):    # 소켓 닫고 부모 클래스에 알리는 함수
         self.bConnect = False
         if hasattr(self, 'client'):
             del(self.client)
             print('Client Stop')
             self.disconnect.disconnect_signal.emit()
 
-    def receive(self, client):
+    def receive(self, client):   # 클라이언트 소켓 연결이 정상적으로 이루어지면 생성되는 쓰레드에 의해 호출되는 함수
+        """
+        무한루프 통해 소켓의 데이터 수신하기 위해 대기하는 코드.
+        반드시 쓰레드로 구성되어야 하며,
+        또 다른 데이터가 언제 수신될 지 모르기 때문에 무한 루프로 구성해 계속 대기할 수 있어야 한다.
+        :param client:  쓰레드에서 받는 클라이언트 소켓
+        :return:
+        """
         while self.bConnect:
             try:
                 recv = client.recv(1024)
@@ -61,6 +73,12 @@ class ClientSocket():
         self.stop()
 
     def send(self, msg):
+        """
+        부모클래스 윈도우의 보내기 버튼을 누르면 호출되는 함수
+        보낼 메시지 내용 복사해 연결된 소켓으로 전송하는 역할을 담당한다.
+        :param mag:  복사된 메시지. 바이너리 데이터로 부호화해서 연결된 소켓으로 전송된다.
+        :return:
+        """
         if not self.bConnect:
             return
         try:
--- client/client_ui.py
+++ client/client_ui.py
@@ -1,18 +1,23 @@
-import sys, signal, socketio
-from PyQt5 import uic, QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import QWidget, QApplication, QTableWidgetItem, QListWidgetItem, QLineEdit, QListWidget
+import sys
+from PyQt5 import uic
+from PyQt5.QtWidgets import QWidget, QApplication, QListWidgetItem
 from PyQt5.QtGui import QIcon
 import client
-from socket import *
 
-ui = uic.loadUiType("client_ui.ui")[0]
+
+ui = uic.loadUiType("client_ui.ui")[0] # PyQt designer로 작성한 ui 파일 불러오기
 
 class client_window(QWidget, ui):
+    """
+    QWidget 으로부터 상속 받아 위젯 만드는 클래스. 불러온 ui 파일 바탕으로 위젯을 생성한다.
+    :__init__:  client.py에서 작성한 ClientSocket 클래스를 호출하며, ClientSocket의 객체를 self.client_socket 이라는 이름으로 생성한다.
+                designer 파일에서 작성한 connect_btn에 setCheckable() 함수를 사용해 True로 설정하여, 버튼을 누른 상태와 그렇지 않은 상태를 구분해준다.
+    """
     def __init__(self):
-        super().__init__()
-        self.setupUi(self)
-        self.client_socket = client.ClientSocket(self)
-        self.connect_btn.setCheckable(True)
+        super().__init__()  # QWidget 으로부터 상속
+        self.setupUi(self)  # 불러온 ui 파일 바탕으로 위젯 생성
+        self.client_socket = client.ClientSocket(self) # client.py에서 작성한 ClientSocket 클래스 호출, ClientSocket의 객체를 self.client_socket이라는 이름으로 생성
+        self.connect_btn.setCheckable(True)  # setCheckable(True) 설정 시, 버튼 누른 상태와 누르지 않은 상태 구분
         self.bConnect = False
 
     def connectClicked(self):
@@ -32,26 +37,26 @@ class client_window(QWidget, ui):
             self.recv_msg.clear()
             self.connect_btn.setText('접속')
 
-    def updateMsg(self, msg):
+    def updateMsg(self, msg): # 보낼 메시지에 작성한 내용을 소켓 클래스로 전달해 데이터 송신하는 역할
         self.connect_btn.setText('접속')
         self.recv_msg.addItem(QListWidgetItem(msg))
 
-    def updateDisconnect(self):
-        self.btn.setText('접속')
+    def updateDisconnect(self): # 소켓 연결 끊어진 경우 호출되는 함수
+        self.btn.setText('접속') # 다시 접속 버튼 누를 수 있도록 setText() 함수 사용해 버튼 내 텍스트 수정
 
     def sendMsg(self):
         send_msg = self.send_msg.toPlainText()
         self.client_socket.send(send_msg)
         self.send_msg.clear()
 
-    def clearMsg(self):
+    def clearMsg(self): # 수신 받은 전체 메시지 삭제
         self.recv_msg.clear()
 
     def closeEvent(self, e):
-        pass
+        self.client_socket.stop()
 
 
-if __name__ == "__main__":
+if __name__ == "__main__":  # main 함수로 윈도우 창 생성하고 프로그램 생성
     app = QApplication(sys.argv)
     chat_window = client_window()
     chat_window.setWindowTitle('oss talk')
